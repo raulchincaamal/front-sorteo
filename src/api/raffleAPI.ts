6718b0a8cdf14e1fc71fa079cd70d01a8eb06df9
Raul Chin Caamal <raul.chim@macropay.mx>
2022-12-13 14:07:50 -0600

    Cambio de host API

--- src/api/raffleAPI.ts
+++ src/api/raffleAPI.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 import { IGift } from "../interfaces";
 
-const BASE_URL_API = "https://z20bt3i5pe.execute-api.us-east-1.amazonaws.com/dev/api";
+const BASE_URL_API = "http://3.80.182.17:3000/api"; // "https://z20bt3i5pe.execute-api.us-east-1.amazonaws.com/dev/api";
 const baseApi = axios.create({
   baseURL: BASE_URL_API,
 });
 
 export const getGifts = async(): Promise<IGift[]> => {
-  const { data } = await baseApi.post(`sorteo/regalo`);
+  const { data } = await baseApi.post(`/sorteo/regalo`);
   return data.data as IGift[];
 }
 
